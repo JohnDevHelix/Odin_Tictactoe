45cf6f60f7ca4834e6c5a5482bbc9e4ad6b24d9c
JohnDevHelix <johndevproject@gmail.com>
2024-12-18 15:24:52 +0800

    Created functions for players, set move to array and eventlistener

--- javascript.js
+++ javascript.js
@@ -1,13 +1,36 @@
-const gameboard = () => {
+function gameboard (move) {
   const boardArray = [];
+  const playerMove = move;
 
-  return { boardArray };
-};
+  const gameMove = (dataSet, move) => {
+    boardArray[dataSet] = move;
+  }
 
-const player = (marker) => {
-  return { marker };
-};
+  return { boardArray, gameMove, playerMove };
+}
 
-const computer = (marker) => {
-  return { marker };
-};
+function player (marker) {
+    const playerOne = marker;
+
+    return { playerOne };
+}
+
+function computer (marker) {
+    const playerTwo = marker;
+
+    return { playerTwo };
+}
+
+document.querySelectorAll(".selectors").forEach((selector) => {
+    selector.addEventListener("click", () => {
+        if (selector.textContent === "X") {
+            const player1 = player("X");
+            const player2 = computer("O");
+            console.log(player1, player2);
+        } else if (selector.textContent === "O") {
+            const player1 = player("O");
+            const player2 = computer("X");
+            console.log(player1, player2);            
+        }
+    })
+})
